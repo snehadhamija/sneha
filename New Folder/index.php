c99c71529b98326653df21bb8e668e70f51a4876
snehadhamija <61445355+snehadhamija@users.noreply.github.com>
2020-03-01 14:35:00 +0530

    Update index.php

--- New Folder/index.php
+++ New Folder/index.php
@@ -1,9 +1,49 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+<?php
+
+$request = new HttpRequest();
+$request->setUrl('http://localhost:8080/sneha/New Folder/beat');
+$request->setMethod(HTTP_METH_GET);
+
+$request->setHeaders(array(
+  'cache-control' => 'no-cache',
+  'Connection' => 'keep-alive',
+  'Content-Length' => '288',
+  'Accept-Encoding' => 'gzip, deflate',
+  'Host' => 'localhost:8080',
+  'Postman-Token' => '25b83b1f-1944-4193-9120-f8b9f07379f9,3324b72e-0f2a-4fd3-aa6d-1cf911b2fcc5',
+  'Cache-Control' => 'no-cache',
+  'Accept' => '/',
+  'User-Agent' => 'PostmanRuntime/7.17.1',
+  'Content-Type' => 'application/json'
+));
+
+$request->setBody('{
+    "fullName": "Sugasini.C",
+    "mobileNumber": "9787647062",
+    "email": "sugasini.c@stanzaliving.com",
+    "hostels": [
+        {
+            "id": 109
+        }
+    ],
+    "userProfiles": [
+        {
+            "id": 18
+        },
+        {
+            "id": 23
+        }
+    ]
+}');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <?php include("HEAD.PHP"); ?>
